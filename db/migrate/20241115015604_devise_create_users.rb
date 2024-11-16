b8a1f01dbf2b0936e7e8bb86a68bd7967b7e4d6d
ymspb <masayayamamoto99@gmail.com>
2024-11-16 17:32:51 +0900

    [Finish] 4-5

--- db/migrate/20241115015604_devise_create_users.rb
+++ db/migrate/20241115015604_devise_create_users.rb
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      # column to save username
+      t.string :name
+
 
       t.timestamps null: false
     end
